761595b6fe7c939ab6b1367f4d5c1e8fee304142
201001058 <devianzendejas@gmail.com>
2023-06-05 11:44:56 -0700

    initial add

--- app/src/main/java/com/poly/madlibs/ChooseMadlibsFragment.java
+++ app/src/main/java/com/poly/madlibs/ChooseMadlibsFragment.java
@@ -83,6 +83,7 @@ public class ChooseMadlibsFragment extends Fragment {
         //construct list of madlibs
         madLibsList = new ArrayList<MadLibsModel>();
         madLibsList.add(new MadLibsModel("Steve's Dream","Steve M", new MadLibStevesDreamActivity(), R.drawable.stevesdream_image));
+        madLibsList.add(new MadLibsModel("Night in the Forest","Devian Zendejas", new MadLibsNightInTheForestActivity(), R.drawable.nightintheforest_image));
 
         //set adapter
         MadLibs_RecyclerViewAdapter adapter = new MadLibs_RecyclerViewAdapter(context, madLibsList);
