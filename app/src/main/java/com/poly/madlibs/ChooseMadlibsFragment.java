de8e5c1aec241aa2e8c7bd280006aace9af0d03c
ryanb <ryanburk3@gmail.com>
2023-06-06 21:26:46 -0700

    sorted madlibs alphabetically

--- app/src/main/java/com/poly/madlibs/ChooseMadlibsFragment.java
+++ app/src/main/java/com/poly/madlibs/ChooseMadlibsFragment.java
@@ -81,21 +81,21 @@ public class ChooseMadlibsFragment extends Fragment {
         //construct list of madlibs
         madLibsList = new ArrayList<ModelMadLib>();
 
-        madLibsList.add(new ModelMadLib("Evan's Choices","Evan Soth", new MadLibsWOOLOO2Activity(), R.drawable.image_evan_cover));
-        madLibsList.add(new ModelMadLib("Jom and Terry","Matthew Yen", new MadLibJomandTerryActivity(), R.drawable.jomterry_image));
-        madLibsList.add(new ModelMadLib("Joe's","Diego Guerra", new MadLibJoesActivity(), R.drawable.joes_image));
-        madLibsList.add(new ModelMadLib("Night in the Forest","Devian Zendejas", new MadLibsNightInTheForestActivity(), R.drawable.nightintheforest_image));
-        madLibsList.add(new ModelMadLib("Toby's Adventure","Giovanni Marcelo", new MadLibTobysAdventureActivity(), R.drawable.tobysadventure_image));
-        madLibsList.add(new ModelMadLib("Dragon Slayer","JD Cajimat", new MadLibDragonSlayerActivity(), R.drawable.dragonslayer_image));
-        madLibsList.add(new ModelMadLib("Fooding","Jovanni Trujillo", new MadLibFoodingActivity(), R.drawable.fooding_image));
-        madLibsList.add(new ModelMadLib("The Birthday Party","Kevin Guzman", new MadLibBirthdayPartyActivity(), R.drawable.birthdayparty_image));
-        madLibsList.add(new ModelMadLib("Steve's Dream","Steve Marquez", new MadLibStevesDreamActivity(), R.drawable.stevesdream_image));
-        madLibsList.add(new ModelMadLib("Roadtrip","Anaiyah Magee", new MadLibRoadTripActivity(), R.drawable.roadtrip_image));
-        madLibsList.add(new ModelMadLib("Concert Night","Hannah Schroeder", new MadLibConcertNightActivity(), R.drawable.concertnight_image));
         madLibsList.add(new ModelMadLib("An Eventful Mugging","Jacob Congolino", new MadLibMuggingActivity(), R.drawable.mugging_image2));
+        madLibsList.add(new ModelMadLib("The Birthday Party","Kevin Guzman", new MadLibBirthdayPartyActivity(), R.drawable.birthdayparty_image));
+        madLibsList.add(new ModelMadLib("Concert Night","Hannah Schroeder", new MadLibConcertNightActivity(), R.drawable.concertnight_image));
+        madLibsList.add(new ModelMadLib("Dragon Slayer","JD Cajimat", new MadLibDragonSlayerActivity(), R.drawable.dragonslayer_image));
+        madLibsList.add(new ModelMadLib("Evan's Choices","Evan Soth", new MadLibsWOOLOO2Activity(), R.drawable.image_evan_cover));
+        madLibsList.add(new ModelMadLib("Fooding","Jovanni Trujillo", new MadLibFoodingActivity(), R.drawable.fooding_image));
+        madLibsList.add(new ModelMadLib("Joe's","Diego Guerra", new MadLibJoesActivity(), R.drawable.joes_image));
+        madLibsList.add(new ModelMadLib("Jom and Terry","Matthew Yen", new MadLibJomandTerryActivity(), R.drawable.jomterry_image));
         madLibsList.add(new ModelMadLib("Just a Regular Day","Kayla Deng-Martinez", new MadLibRegularDayActivity(), R.drawable.regularday_image));
-        madLibsList.add(new ModelMadLib("Summer Vacation","Lex Battles", new MadLibSummerVacationActivity(), R.drawable.summervacation_image));
         madLibsList.add(new ModelMadLib("Maria's House","Rafael Valencia", new MadLibRafaelActivity(), R.drawable.rafael_image));
+        madLibsList.add(new ModelMadLib("Night in the Forest","Devian Zendejas", new MadLibsNightInTheForestActivity(), R.drawable.nightintheforest_image));
+        madLibsList.add(new ModelMadLib("Roadtrip","Anaiyah Magee", new MadLibRoadTripActivity(), R.drawable.roadtrip_image));
+        madLibsList.add(new ModelMadLib("Steve's Dream","Steve Marquez", new MadLibStevesDreamActivity(), R.drawable.stevesdream_image));
+        madLibsList.add(new ModelMadLib("Summer Vacation","Lex Battles", new MadLibSummerVacationActivity(), R.drawable.summervacation_image));
+        madLibsList.add(new ModelMadLib("Toby's Adventure","Giovanni Marcelo", new MadLibTobysAdventureActivity(), R.drawable.tobysadventure_image));
 
         //set adapter
         MadLibs_RecyclerViewAdapter adapter = new MadLibs_RecyclerViewAdapter(context, madLibsList);
