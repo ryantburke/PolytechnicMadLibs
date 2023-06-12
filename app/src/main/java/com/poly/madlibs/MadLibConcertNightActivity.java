c13dd65d47faa02baa71b2d68d57e4ea0132391d
ryanb <ryanburk3@gmail.com>
2023-06-11 21:39:18 -0700

    updated ConcertNight

--- app/src/main/java/com/poly/madlibs/MadLibConcertNightActivity.java
+++ app/src/main/java/com/poly/madlibs/MadLibConcertNightActivity.java
@@ -32,21 +32,25 @@ public class MadLibConcertNightActivity extends AppCompatActivity {
         EditText clothingItem = findViewById(R.id.clothingItem);
         EditText sport = findViewById(R.id.sport);
 
+        EditText[] etAll = {name, adjective, verb, musicGenre, modeOfTransport, relationshipType, animal, season, clothingItem, sport};
+
+        MadLibUtil.prepareStory(etAll,generateMadlib);
+
         generateMadlib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                String story = "You just got tickets to see your favorite artist, " + name.getText() + " in concert tonight!";
-                story += "\nYou are very excited because the artist is extremely " + adjective.getText() + " and always " + verb.getText() + "s on stage.";
-                story += "\nYou usually do not like " + musicGenre.getText() + " music, but you make an exception because the tickets were free and you are cheap.";
-                story += "\nYou and your " + relationshipType.getText() + " try to drive to the concert, but your car dies on the way there.";
-                story += "\nAfter being stuck on the freeway for 20 minutes, you and your " + relationshipType.getText() + " decide to ditch the car and take a " + modeOfTransport.getText() + " instead.";
-                story += "\nYou guys are on your way again, until you spot a " + animal.getText() + " in the middle of the road";
-                story += "\nYou are confused because you never finished school and you thought that all " + animal.getText() + "s died in the " + season.getText() + ".";
-                story += "\nThe " + animal.getText() + verb.getText() + "s up to you and bites you, ripping your favorite " + clothingItem.getText() + ".";
-                story += "\nYou are so frustrated that you don't even want to go to " + name.getText() + "'s concert anymore.";
-                story += "\nYou see that a group of strangers started a game of " + sport.getText() + ", so you ditch your " + relationshipType.getText() + " and join them instead.";
+                String story = "You just got tickets to see your favorite artist, " + name.getText() + " in concert tonight! ";
+                story += "You are very excited because the artist is extremely " + adjective.getText() + " and always " + verb.getText() + "s on stage. ";
+                story += "You usually do not like " + musicGenre.getText() + " music, but you make an exception because the tickets were free and you are cheap. ";
+                story += "You and your " + relationshipType.getText() + " try to drive to the concert, but your car dies on the way there. ";
+                story += "After being stuck on the freeway for 20 minutes, you and your " + relationshipType.getText() + " decide to ditch the car and take a " + modeOfTransport.getText() + " instead. ";
+                story += "You guys are on your way again, until you spot a " + animal.getText() + " in the middle of the road. ";
+                story += "You are confused because you never finished school and you thought that all " + animal.getText() + "s died in the " + season.getText() + ". ";
+                story += "The " + animal.getText() + " " + verb.getText() + "s up to you and bites you, ripping your favorite " + clothingItem.getText() + ". ";
+                story += "You are so frustrated that you don't even want to go to " + name.getText() + "'s concert anymore. ";
+                story += "You see that a group of strangers started a game of " + sport.getText() + ", so you ditch your " + relationshipType.getText() + " and join them instead. ";
 
 
                 secondActivity.putExtra("story", story);
